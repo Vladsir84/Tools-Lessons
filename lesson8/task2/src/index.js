8fcb8c08ea495eaefe1a9d250e71e45ea6867c66
John Doe <vperedz@gmail.com>
2020-02-08 19:09:48 +0200

    add

---
+++ lesson8/task2/src/index.js
@@ -0,0 +1,5 @@
+import { addTask } from './list/addTask.js';
+import { createTask } from './list/createTask.js';
+import { renderListElements } from './list/renderer.js';
+import { onStorageChange, onDocumentLoaded } from './list/storage.js';
+import './list/index.scss';
